c0315920e1351863e8c756124f38aa8b2ff04fe8
vaxich <vaxich@rambler.ru>
2022-01-19 09:52:56 +0300

    пишем что делели

---
+++ my-app/src/01-hello-test/01.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export function sum(a:number, b:number) {
+    return a + b
+}
+
+export function mult(a:number, b:number) {
+    return a * b
+}
+
+
+
+
+export function splitIntoWords(sentense: string) {
+    return ["hello", "my", "friends"]
+}
